8f4d96b9251ba5973b5dd1119fbe70cc21fbfeca
김수빈 <chfhrdl0001@naver.com>
2022-05-18 13:51:43 +0900

    settings.py 수정

--- Back-end/project_back_end/settings.py
+++ Back-end/project_back_end/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     # 3rd party
     'django_extensions',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
     
     # DRF auth
     'dj_rest_auth',
