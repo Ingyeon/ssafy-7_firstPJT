63a6858c3efa4f592d45f49fe89f27963776d73c
최용욱 <qwe0274@naver.com>
2022-05-18 13:39:37 +0900

    최초

---
+++ Back-end/movies/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import get_object_or_404
+from django.db.models import Count
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
