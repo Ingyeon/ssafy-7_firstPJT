40dd4fca7a081f9fd8f753ab6701b9ee769dedae
김수빈 <chfhrdl0001@naver.com>
2022-05-20 16:56:54 +0900

    re

--- Front-end/project_front_end/src/store/index.js
+++ Front-end/project_front_end/src/store/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// import articles from './modules/articles'
 import accounts from './modules/accounts'
 import movies from './modules/movies'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-<<<<<<< HEAD
   modules: { accounts, movies }, 
 })
-=======
-  modules: { accounts },
-})
->>>>>>> subin-front_fix
