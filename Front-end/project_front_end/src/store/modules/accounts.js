a5e6e30447c508bc3da054b4274858a28244419d
김수빈 <chfhrdl0001@naver.com>
2022-05-20 14:27:09 +0900

    로그인 에러 수정 전 commit

---
+++ Front-end/project_front_end/src/store/modules/accounts.js
@@ -0,0 +1,74 @@
+import router from '@/router'
+
+
+export default {
+  state: {
+    token: localStorage.getItem('token') || '',
+    currentUser: {},
+    profile: {},
+    authError: null,
+  },
+  
+  getters: {
+    isLoggedIn: state => !!state.token,
+    currentUser: state => state.currentUser,
+    profile: state => state.profile,
+    authError: state => state.authError,
+    authHeader: state => ({ Authorization: `Token ${state.token}` }),
+  },
+
+  mutations: {
+    SET_TOKEN: (state, token) => state.token = token,
+    SET_CURRENT_USER: (state, user) => state.currentUser = user,
+    SET_PROFILE: (state, profile) => state.profile = profile,
+    SET_AUTH_ERROR: (state, error) => state.authError = error
+  },
+
+  actions: {
+    saveToken({ commit }, token) {
+      /* 
+      state.token 추가 
+      localStorage에 token 추가
+      */
+      commit('SET_TOKEN', token)
+      localStorage.setItem('token', token)
+    },
+
+    removeToken({ commit }) {
+      /* 
+      state.token 삭제
+      localStorage에 token 추가
+      */
+      commit('SET_TOKEN', '')
+      localStorage.setItem('token', '')
+    },
+
+    login({ commit, dispatch }, credentials) {
+      /* 
+      POST: 사용자 입력정보를 login URL로 보내기
+        성공하면
+          응답 토큰 저장
+          현재 사용자 정보 받기
+          메인 페이지(ArticleListView)로 이동
+        실패하면
+          에러 메시지 표시
+      */
+      axios({
+        url: 'http://localhost:8000/api/v1/accounts/login/',
+        // drf.accounts.login(), // url 주소 -> drf 만들어지면 바꿀거임
+        method: 'post',
+        data: credentials
+      })
+        .then(res => {
+          const token = res.data.key
+          dispatch('saveToken', token)
+          // dispatch('fetchCurrentUser')
+          router.push({ name: 'articles' })
+        })
+        .catch(err => {
+          console.error(err.response.data)
+          commit('SET_AUTH_ERROR', err.response.data)
+        })
+    },
+  },
+}
