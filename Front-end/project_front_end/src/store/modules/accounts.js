e44572131d32c81fe19e2465bf173a912e746bd0
김수빈 <chfhrdl0001@naver.com>
2022-05-20 16:34:22 +0900

    accounts 수정

--- Front-end/project_front_end/src/store/modules/accounts.js
+++ Front-end/project_front_end/src/store/modules/accounts.js
@@ -2,20 +2,22 @@ import router from '@/router'
 import axios from 'axios'
 import drf from '@/api/drf'
 
+
 export default {
+
   state: {
-    token: localStorage.getItem('token') || '',
+    token: localStorage.getItem('token') || '' ,
     currentUser: {},
     profile: {},
     authError: null,
   },
-  
+ 
   getters: {
     isLoggedIn: state => !!state.token,
     currentUser: state => state.currentUser,
     profile: state => state.profile,
     authError: state => state.authError,
-    authHeader: state => ({ Authorization: `Token ${state.token}` }),
+    authHeader: state => ({ Authorization: `Token ${state.token}`})
   },
 
   mutations: {
@@ -27,42 +29,26 @@ export default {
 
   actions: {
     saveToken({ commit }, token) {
-      /* 
-      state.token 추가 
-      localStorage에 token 추가
-      */
       commit('SET_TOKEN', token)
       localStorage.setItem('token', token)
     },
 
     removeToken({ commit }) {
-      /* 
-      state.token 삭제
-      localStorage에 token 추가
-      */
       commit('SET_TOKEN', '')
       localStorage.setItem('token', '')
     },
 
     login({ commit, dispatch }, credentials) {
-      /* 
-      POST: 사용자 입력정보를 login URL로 보내기
-        성공하면
-          응답 토큰 저장
-          현재 사용자 정보 받기
-          메인 페이지(ArticleListView)로 이동
-        실패하면
-          에러 메시지 표시
-      */
+      
       axios({
-        url: drf.accounts.login(), // url 주소 -> drf 만들어지면 바꿀거임
+        url: drf.accounts.login(),
         method: 'post',
         data: credentials
       })
         .then(res => {
           const token = res.data.key
           dispatch('saveToken', token)
-          // dispatch('fetchCurrentUser')
+          dispatch('fetchCurrentUser')
           router.push({ name: 'articles' })
         })
         .catch(err => {
@@ -70,16 +56,9 @@ export default {
           commit('SET_AUTH_ERROR', err.response.data)
         })
     },
+
     signup({ commit, dispatch }, credentials) {
-      /* 
-      POST: 사용자 입력정보를 signup URL로 보내기
-        성공하면
-          응답 토큰 저장
-          현재 사용자 정보 받기
-          메인 페이지(ArticleListView)로 이동
-        실패하면
-          에러 메시지 표시
-      */
+      
       axios({
         url: drf.accounts.signup(),
         method: 'post',
@@ -88,7 +67,7 @@ export default {
         .then(res => {
           const token = res.data.key
           dispatch('saveToken', token)
-          //dispatch('fetchCurrentUser')
+          dispatch('fetchCurrentUser')
           router.push({ name: 'articles' })
         })
         .catch(err => {
@@ -97,5 +76,52 @@ export default {
         })
     },
 
+    logout({ getters, dispatch }) {
+      
+      axios({
+        url: drf.accounts.logout(),
+        method: 'post',
+        // data: {},
+        headers: getters.authHeader,
+      })
+        .then(() => {
+          dispatch('removeToken')
+          alert('성공적으로 logout!')
+          router.push({ name: 'login' })
+        })
+        .error(err => {
+          console.error(err.response)
+        })
+    },
+
+    fetchCurrentUser({ commit, getters, dispatch }) {
+      
+      if (getters.isLoggedIn) {
+        axios({
+          url: drf.accounts.currentUserInfo(),
+          method: 'get',
+          headers: getters.authHeader,
+        })
+          .then(res => commit('SET_CURRENT_USER', res.data))
+          .catch(err => {
+            if (err.response.status === 401) {
+              dispatch('removeToken')
+              router.push({ name: 'login' })
+            }
+          })
+      }
+    },
+
+    fetchProfile({ commit, getters }, { username }) {
+      
+      axios({
+        url: drf.accounts.profile(username),
+        method: 'get',
+        headers: getters.authHeader,
+      })
+        .then(res => {
+          commit('SET_PROFILE', res.data)
+        })
+    },
   },
 }
