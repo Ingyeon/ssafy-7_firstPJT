ecf9b690781efc5d81ff1528caf852ff3df35e71
최용욱 <qwe0274@naver.com>
2022-05-25 01:21:25 +0900

    필드 null 허용

--- Back-end/movies/models.py
+++ Back-end/movies/models.py
@@ -4,13 +4,13 @@ from django.db import models
 # Create your models here.
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    release_date = models.DateField()
-    popularity = models.FloatField()
-    overview = models.TextField()
-    poster_path = models.CharField(max_length=200)
+    title = models.CharField(max_length=100, null=True)
+    release_date = models.DateField(null=True)
+    popularity = models.FloatField(null=True)
+    overview = models.TextField(null=True)
+    poster_path = models.CharField(max_length=200, null=True)
     movie_id = models.IntegerField(primary_key=True)
-    genre_id = models.IntegerField()
+    genre_id = models.IntegerField(null=True)
     movie_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='movie')
 
 
