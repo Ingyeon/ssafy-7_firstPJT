66327e1c72231a05f0cda07b56e927f0619ee957
김수빈 <chfhrdl0001@naver.com>
2022-05-25 14:46:37 +0900

    tmp

--- Front-end/project_front_end/src/views/CommunityView.vue
+++ Front-end/project_front_end/src/views/CommunityView.vue
@@ -4,9 +4,13 @@
     <!-- movieid 랑 매핑 안됨 -->
     <!-- <router-link :to="{ name: 'reviewCreate' }">리뷰 작성</router-link> -->
     <hr>
+    <!-- <div>
+      <b-table striped hover :items="items"></b-table>
+    </div> -->
     <ul>
       <li v-for="review in reviews" :key="review.pk">
         <!-- 작성자 -->
+        
         {{ review.user.username }} : 
 
         <!-- 글 이동 링크 (제목) -->
@@ -34,6 +38,24 @@
     components: {
       // board
     },
+
+    // data(){
+    //   const items = []
+    //   const rvs = this.reviews
+
+    //   // for (let i ; i < rvs.lenth ; i++) {
+    //   //   items.push({ 
+    //   //   })
+    //   // }
+
+
+    //   return {
+    //     items : [
+
+    //     ]
+    //   }
+    // },
+
     computed: {
       ...mapGetters(['reviews'])
     },
